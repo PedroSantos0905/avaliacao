a9f33088338d3421f01e0cdef34e177c55dc1ac5
Pedro Santos <pedrosantos0509@gmail.com>
2021-02-14 19:52:39 -0300

    Votos em relação ao total de eleitores

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const rotaVotos = require('./class/votos');
+
+app.use('/votos', rotaVotos);
+
 app.use((req, res, next) => {
     res.status(200).send({
         mensagem: 'Ok!'
