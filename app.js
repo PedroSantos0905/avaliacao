0f710ff72c71136524e66a51ff7ca8553fefe58b
Pedro Santos <pedrosantos0509@gmail.com>
2021-02-14 20:45:19 -0300

    Ordenação de vetor com bubble sort

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 const rotaVotos = require('./class/votos');
+const rotaVetor = require('./class/vetor');
 
 app.use('/votos', rotaVotos);
+app.use('/vetor', rotaVetor);
 
 app.use((req, res, next) => {
     res.status(200).send({
